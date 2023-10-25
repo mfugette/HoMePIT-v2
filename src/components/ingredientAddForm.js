f96f432f79f338e95d8ae4b4efe0c9f572a58530
mfugette <mfugette.student@gmail.com>
2023-10-25 10:33:22 -0500

    made ingredientAddForm a popup

--- src/components/ingredientAddForm.js
+++ src/components/ingredientAddForm.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 //import { useState } from "react"
 
 //const [ingredientQuantity, setIngredientQuantity] = React.useState(0);
 
-export default function IngredientAddForm({onAddIngredient}) {
+export default function IngredientAddForm({ onAddIngredient }) {
     const [name, setName] = React.useState("");
     const [quantity, setQuantity] = React.useState('');
 
@@ -17,12 +19,17 @@ export default function IngredientAddForm({onAddIngredient}) {
     }
 
     return (
-        <form onSubmit={handleSubmit} className="add">
-            <h3>Add Ingredient</h3>
-            <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-            <input type="text" placeholder="Quantity" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} />
-            <button type="submit">Add Ingredient</button>
 
-        </form>
+        <Popup trigger={<button>Add New Ingredient</button>} position="right center">
+
+            <form onSubmit={handleSubmit} className="add">
+                <h3>Input Food:</h3>
+                <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" placeholder="Quantity" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))} />
+                <button type="submit">Add Ingredient</button>
+            </form>
+
+        </Popup>
+
     )
 }
