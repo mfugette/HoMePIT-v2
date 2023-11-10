cd5e51f2c1fc8d07d729fbb71aff2c9afb2585c0
Maxwell Fugette <mfugette.student@gmail.com>
2023-11-10 13:52:52 -0600

    simplified Pantry; updated RecipeBook

---
+++ src/components/pantryComponents/ingredientForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import Pantry from "@/pages/pantry";
+
+export default function IngredientForm() {
+
+    // return (
+    //     <div>
+
+    //         <form onSubmit={createIngredient} className="add">
+
+    //             <h3>Input Food</h3>
+    //             <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+    //             <input type="text" placeholder="Quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+    //             <input type="text" placeholder="Threshold" value={threshold} onChange={(e) => setThreshold(e.target.value)} />
+    //             <input type="text" placeholder="Serving Size" value={servingSize} onChange={(e) => setServingSize(e.target.value)} />
+    //             <input type="text" placeholder="Calories" value={calories} onChange={(e) => setCalories(e.target.value)} />
+    //             <input type="text" placeholder="Protein" value={protein} onChange={(e) => setProtein(e.target.value)} />
+    //             <input type="text" placeholder="Fat" value={fat} onChange={(e) => setFat(e.target.value)} />
+    //             <input type="text" placeholder="Carbohydrate" value={carbohydrate} onChange={(e) => setCarbohydrate(e.target.value)} />
+    //             <input type="text" placeholder="Purchased Servings" value={purchasedServings} onChange={(e) => setPurchasedServings(e.target.value)} />
+    //             <input type="text" placeholder="Cost" value={cost} onChange={(e) => setCost(e.target.value)} />
+
+    //             <button type="submit">Add Ingredient</button>
+    //         </form>
+
+    //     </div>
+    // )
+}
--- src/pages/recipeBook.js
+++ src/pages/recipeBook.js
@@ -1,44 +1,177 @@
-import RecipeAddForm from '@/components/recipeComponents/recipeAddForm';
 import React from 'react';
-
-
+import { useEffect } from 'react';
+import supabase from '@/config/supabaseClient.js';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 export default function RecipeBook() {
 
-
     const [recipes, setRecipes] = React.useState([]);
+    const [name, setName] = React.useState('');
+    const [servingCount, setServingCount] = React.useState();
+    const [indCookTime, setIndCookTime] = React.useState();
+    const [totalCookTime, setTotalCookTime] = React.useState();
+    const [servingCalories, setServingCalories] = React.useState();
+    const [servingProtein, setServingProtein] = React.useState();
+    const [servingFat, setServingFat] = React.useState();
+    const [servingCarbohydrate, setServingCarbohydrate] = React.useState();
+    const [servingCost, setServingCost] = React.useState();
+    const [timeSinceLastEaten, setTimeSinceLastEaten] = React.useState();
 
-    const handleAddRecipe = (
-        name,
-        servingCount,
-        cookTime,
-        macrosPerServing,
-        predictedExpiry,
-        timeSinceLastCooked,
-        userTags,
-        ingredients
-    ) => {
+    const createRecipe = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await supabase
+                .from('Recipes')
+                .insert([
+                    {
+                        rec_name: name,
+                        rec_serv_count: servingCount,
+                        rec_ind_cook_time: indCookTime,
+                        rec_total_cook_time: totalCookTime,
+                        rec_serv_cal: servingCalories,
+                        rec_serv_prot: servingProtein,
+                        rec_serv_fat: servingFat,
+                        rec_serv_carb: servingCarbohydrate,
+                        rec_serv_cost: servingCost,
+                        rec_time_since_eaten: timeSinceLastEaten,
+                    }
+                ]);
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-        const recipe = {
-            id: self.crypto.randomUUID(),
-            name,
-            servingCount,
-            cookTime,
-            macrosPerServing,
-            predictedExpiry,
-            timeSinceLastCooked,
-            userTags,
-            ingredients
+    const updateRecipe = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await supabase
+                .from('Recipes')
+                .update([
+                    {
+                        rec_name: name,
+                        rec_serv_count: servingCount,
+                        rec_ind_cook_time: indCookTime,
+                        rec_total_cook_time: totalCookTime,
+                        rec_serv_cal: servingCalories,
+                        rec_serv_prot: servingProtein,
+                        rec_serv_fat: servingFat,
+                        rec_serv_carb: servingCarbohydrate,
+                        rec_serv_cost: servingCost,
+                        rec_time_since_eaten: timeSinceLastEaten,
+                    }
+                ])
+                .select();
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        const readRecipe = async () => {
+            try {
+                const response = await supabase
+                    .from('Recipes')
+                    .select('rec_id, rec_name, rec_serv_count')
+                // .range(0, 4)
+                const data = response.data;
+                setRecipes(data);
+            } catch (error) {
+                console.error(error);
+            }
         };
-        setRecipes([...recipes, recipe]);
+        readRecipe();
+    }, []);
+
+    const deleteRecipe = async (id) => {
+        try {
+            const response = await supabase
+                .from('Recipes')
+                .delete()
+                .eq('rec_id', id)
+            const data = response.data;
+            setRecipes(data);
+            location.reload();
+        } catch (error) {
+            console.error(error);
+        }
     }
+
     return (
-        <div>
-            <h1>RecipeBook</h1>
+        <>
 
-        <RecipeAddForm onAddRecipe={handleAddRecipe}></RecipeAddForm>
+            <div>
+                <div>
+                    <Popup trigger={<button>Add New Recipe</button>} position={"bottom right"}>
 
-        </div>
+                        <form onSubmit={createRecipe} className="add">
+                            <h3>Input Recipe Data</h3>
+                            <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                            <input type="text" placeholder="Serving Count" value={servingCount} onChange={(e) => setServingCount(e.target.value)} />
+                            <input type="text" placeholder="Ind Cook Time" value={indCookTime} onChange={(e) => setIndCookTime(e.target.value)} />
+                            <input type="text" placeholder="Total Cook Time" value={totalCookTime} onChange={(e) => setTotalCookTime(e.target.value)} />
+                            <input type="text" placeholder="Total Calories" value={servingCalories} onChange={(e) => setServingCalories(e.target.value)} />
+                            <input type="text" placeholder="Total Protein" value={servingProtein} onChange={(e) => setServingProtein(e.target.value)} />
+                            <input type="text" placeholder="Total Fat" value={servingFat} onChange={(e) => setServingFat(e.target.value)} />
+                            <input type="text" placeholder="Total Carbohydrate" value={servingCarbohydrate} onChange={(e) => setServingCarbohydrate(e.target.value)} />
+                            <input type="text" placeholder="Serving Cost" value={servingCost} onChange={(e) => setServingCost(e.target.value)} />
+                            <input type="text" placeholder="Time Since Last Eaten" value={timeSinceLastEaten} onChange={(e) => setTimeSinceLastEaten(e.target.value)} />
+
+                            <button type="submit">Add Recipe</button>
+                        </form>
+
+                    </Popup>
+                    <h3>Your Recipes:</h3>
+                </div>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Servings</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {recipes.map((recipe) => (
+                            <tr key={recipe.rec_id}>
+                                <td>{recipe.rec_name}</td>
+                                <td>{recipe.rec_serv_count}</td>
+
+                                <td>
+                                    {/* <button>Edit</button> */}
+                                    <Popup trigger={<button>Edit</button>} position={"bottom right"}>
+
+                                        <form onSubmit={updateRecipe} className="add">
+                                            <h3>Edit Recipe Data</h3>
+                                            <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                                            <input type="text" placeholder="Serving Count" value={servingCount} onChange={(e) => setServingCount(e.target.value)} />
+                                            <input type="text" placeholder="Ind Cook Time" value={indCookTime} onChange={(e) => setIndCookTime(e.target.value)} />
+                                            <input type="text" placeholder="Total Cook Time" value={totalCookTime} onChange={(e) => setTotalCookTime(e.target.value)} />
+                                            <input type="text" placeholder="Total Calories" value={servingCalories} onChange={(e) => setServingCalories(e.target.value)} />
+                                            <input type="text" placeholder="Total Protein" value={servingProtein} onChange={(e) => setServingProtein(e.target.value)} />
+                                            <input type="text" placeholder="Total Fat" value={servingFat} onChange={(e) => setServingFat(e.target.value)} />
+                                            <input type="text" placeholder="Total Carbohydrate" value={servingCarbohydrate} onChange={(e) => setServingCarbohydrate(e.target.value)} />
+                                            <input type="text" placeholder="Serving Cost" value={servingCost} onChange={(e) => setServingCost(e.target.value)} />
+                                            <input type="text" placeholder="Time Since Last Eaten" value={timeSinceLastEaten} onChange={(e) => setTimeSinceLastEaten(e.target.value)} />
+
+                                            <button type="submit">Edit Recipe</button>
+                                        </form>
+
+                                    </Popup>
+                                    <button onClick={() => deleteIngredient(ingredient.ing_id)}>Remove</button>
+                                </td>
+
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+
+
+        </>
+
     );
-
 }
