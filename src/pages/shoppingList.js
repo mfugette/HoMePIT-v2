d2e30460d6c9613bea352d00fc4eaf41281375b3
jj <jchestnut1@my.apsu.edu>
2023-12-10 22:31:04 -0600

    Minor update to CSS

--- src/pages/shoppingList.js
+++ src/pages/shoppingList.js
@@ -1,6 +1,6 @@
 export default function ShoppingList() {
   return (
-    <div>
+    <div id="sLTitle" style={{ maxWidth: "100%", padding: "1%", textAlign: "center", verticalAlign: "middle" }}>
       <h1>ShoppingList</h1>
     </div>
   );
